63e935519a4719c1895c68260d4cdde193d875ae
JaeJunPark <35682191+JaeJun-Park@users.noreply.github.com>
2021-01-14 13:58:12 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var max_item = 3;
-var current_item = 0;
+var max_item = 1000;
 var button = document.getElementById('enter');
 var input = document.getElementById('userinput');
 var ul = document.querySelector('ul');
@@ -9,28 +8,17 @@ function inputLength() {
 }
 
 function createListElement() {
-	if (current_item >= max_item) {
-		alert("your shopping list is full!!!");
-		return;
-	}
 
 	var li = document.createElement("li");
-	li.style.display = "inline";
 	li.appendChild(document.createTextNode(input.value));
+	var deleteBtn = document.createElement("button");
+	deleteBtn.appendChild(document.createTextNode("x"));
+	deleteBtn.addEventListener("click", function() {ul.removeChild(li);});
+	li.appendChild(deleteBtn);
+
 	li.addEventListener("click", function() {li.classList.toggle("done");});
 	ul.appendChild(li);
 	input.value = "";
-
-	/* add button next to the new list */
-	var del_btn = document.createElement("button");
-	del_btn.appendChild(document.createTextNode("x"));
-	del_btn.addEventListener("click", function() {li.remove(); del_btn.remove(); current_item--;});
-	ul.insertBefore(del_btn, li.nextSibling);
-
-	ul.insertBefore(document.createElement("br"), del_btn.nextSibling);
-
-	current_item++;
-	console.log(current_item+'/'+max_item);
 }
 
 function addListAfterClick() {
